f765d164d7f15c483d5f9685bf9cf6b4fa0e23c4
vanloc0301 <locha9066@gmail.com>
2015-06-10 01:40:34 +0700

    Add product model

---
+++ EF Code first/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_Code_first.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ EF Code first/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EF_Code_first.Startup))]
+namespace EF_Code_first
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
